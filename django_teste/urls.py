034564e2c3cf75af896144b0d925f898c0a00b96
iand <gokugohan081@gmail.com>
2017-08-30 16:49:55 -0300

    Leves modificacoes no HTML

--- django_teste/urls.py
+++ django_teste/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from django_teste.views import sitezao
+#from django_teste.views import sitezao  >> ESSE JEITO NÃO É O MAIS PRÁTICO
+from . import views
 
 urlpatterns = [
-	url(r'^$', sitezao),
+	url(r'^$', views.sitezao),
 ]
