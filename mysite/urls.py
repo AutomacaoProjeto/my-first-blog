dc6586d33c7dde94fba7ab9c51763b931e447820
iand <gokugohan081@gmail.com>
2017-09-28 16:34:21 -0300

    asdfas

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
-
     url(r'^bitch/', include('django_teste.urls')),
-
 ]
